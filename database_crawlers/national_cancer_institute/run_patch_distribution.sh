b64d580f6ceb20ed9603e149dc0ecd1e2fffb50d
amirmosio <amir.mn.mosio@gmail.com>
2022-07-12 01:05:24 +0430

    run patch distribution.sh

--- database_crawlers/national_cancer_institute/run_patch_distribution.sh
+++ database_crawlers/national_cancer_institute/run_patch_distribution.sh
@@ -1,4 +1,5 @@
 export PYTHONPATH="${PYTHONPATH}:../../";
 export PYTHONPATH="${PYTHONPATH}:../";
+export PYTHONPATH="${PYTHONPATH}:../image_patcher";
 export PYTHONPATH="${PYTHONPATH}:./";
 python patch_distribution.py;
