ae8f92ab07cab5372c3b29dc16999ef2290b5e26
amirmosio <amir.mn.mosio@gmail.com>
2022-07-20 00:28:48 +0430

    add runner

---
+++ database_crawlers/image_patcher/test_image_patcher/nci_svs_and_masks/run_patch_evaluator_and_visualizer.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH="${PYTHONPATH}:../../../";
+export PYTHONPATH="${PYTHONPATH}:../../";
+export PYTHONPATH="${PYTHONPATH}:../";
+export PYTHONPATH="${PYTHONPATH}:./";
+python evaluate_image_patcher_and_visualize.py;
