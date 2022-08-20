a8e046c3fdcef0290582d8d0f55bb33915fdb3a0
amirmosio <amir.mn.mosio@gmail.com>
2022-08-21 00:17:28 +0430

    recreate cell distribution chart

--- database_crawlers/national_cancer_institute/cells_chart.py
+++ database_crawlers/national_cancer_institute/cells_chart.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     slide_infos = {}
     for xml_path in pathlib.Path(data_dir).glob("**/*.xml"):
         slide_infos.update(get_slide_info_from_bcr_xml(str(xml_path)))
-    cell_percents = [int(item[1]) for item in slide_infos.values()]
+    cell_percents = [int(item[1]) for item in slide_infos.values() if int(item[2]) == 0]
     plt.hist(cell_percents, bins=150)
     plt.savefig("cell_distribution.jpeg")
