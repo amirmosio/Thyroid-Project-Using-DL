19fa38cfa706ba1fd196018a8518a6476a0efe1c
amirmosio <amir.mn.mosio@gmail.com>
2022-08-22 22:41:44 +0430

    debug training

--- classification_stuff/test_methods/test_model_for_bengin_malignant/model_training.py
+++ classification_stuff/test_methods/test_model_for_bengin_malignant/model_training.py
@@ -306,8 +306,8 @@ if __name__ == '__main__':
         val_percent=0)
     sample_source_domain_test_ds = ThyroidDataset(sample_source_domain_test, Config.class_idx_dict)
     datasets_folder = ["bio_atlas_at_jake_gittlen_laboratories",
-                       # "papsociaty",
-                       # "stanford_tissue_microarray"
+                       "papsociaty",
+                       "stanford_tissue_microarray"
                        ]
     _, _, test = CustomFragmentLoader(datasets_folder).load_image_path_and_labels_and_split(
         test_percent=100,
