8fcb4a3d99ea374804746f9e8b9db5d9a615e7cd
amirmosio <amir.mn.mosio@gmail.com>
2022-09-23 14:59:12 +0330

    add eval function for evaluate nci data

--- classification_stuff/test_methods/test_model_for_bengin_malignant/model_training.py
+++ classification_stuff/test_methods/test_model_for_bengin_malignant/model_training.py
@@ -311,6 +311,7 @@ def evaluate_nci_dataset_per_slide(config_base_name, augmentation, base_model, d
 
     y_targets = []
     y_preds = []
+    print(slides_preds)
     for key, value in slides_preds.items():
         slides_preds[key] = (sum(slides_preds[key]) / len(slides_preds[key])) * 100
         y_preds.append(slides_preds[key])
